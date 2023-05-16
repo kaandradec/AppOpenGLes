bbf91d5aadb2a2a30b884a0272fc85f29cbbd078
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-05-15 19:02:05 -0500

    Color Pantalla

--- app/src/main/java/ec/edu/uce/pa/activities/ColorPantallaActivity.java
+++ app/src/main/java/ec/edu/uce/pa/activities/ColorPantallaActivity.java
@@ -1,13 +1,25 @@
 package ec.edu.uce.pa.activities;
 
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import ec.edu.uce.pa.renderers.RenderColores;
+
 public class ColorPantallaActivity extends AppCompatActivity {
+
+    private GLSurfaceView view;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        view = new GLSurfaceView(this);//Permite manejo de los renderes a utilizar
+        view.setEGLContextClientVersion(1);
+        view.setRenderer(new RenderColores());
+        setContentView(view);
+
     }
 }
