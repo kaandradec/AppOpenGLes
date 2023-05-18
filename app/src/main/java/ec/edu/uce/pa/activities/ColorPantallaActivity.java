39cc0047f8540ec25a14ec75806cebd7548aaff0
kevoaac <130163890+kevoaac@users.noreply.github.com>
2023-05-18 12:02:48 -0500

    create onTouchEvent to change color screen on ColorPantallaActivity

--- app/src/main/java/ec/edu/uce/pa/activities/ColorPantallaActivity.java
+++ app/src/main/java/ec/edu/uce/pa/activities/ColorPantallaActivity.java
@@ -3,6 +3,7 @@ package ec.edu.uce.pa.activities;
 import android.content.Intent;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +13,10 @@ import ec.edu.uce.pa.renderers.RenderColores;
 public class ColorPantallaActivity extends AppCompatActivity {
 
     private GLSurfaceView view;
+    public static float red = 0f;
+    public static float green = 0f;
+    public static float blue = 0f;
+    public static float alpha = 0f;
 
 
     @Override
@@ -21,10 +26,22 @@ public class ColorPantallaActivity extends AppCompatActivity {
         view = new GLSurfaceView(this);//Permite manejo de los renderes a utilizar
         view.setEGLContextClientVersion(1);
         view.setRenderer(new RenderColores());
+        //Para ahorrar recursos cuando solo la geometria cambia
+        //view.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
         setContentView(view);
+    }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
 
-
+        switch (e.getAction()){
+            case MotionEvent.ACTION_UP:
+                red = (float)Math.random();
+                green = (float)Math.random();
+                blue = (float)Math.random();
+                alpha = (float)Math.random();
+        }
+        return super.onTouchEvent(e);
     }
 
     //Boton back de vista ColorPantalla
